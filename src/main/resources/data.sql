710cd508fd62ecce062b03f7c948e636cc05b46d
vaijenyavaiju1 <vaijenyavaiju123@gamil.com>
2024-04-01 04:29:59 +0000

    solution

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO Product(productId, productName,	price) VALUES(1, 'Smartphone', 599.99);
+INSERT INTO Product(productId, productName,	price) VALUES(2, 'Laptop', 1299.99);
+INSERT INTO Product(productId, productName,	price) VALUES(3, 'Gaming Console', 399.99);
+
+INSERT INTO Review(reviewContent, rating, productId) VALUES('Great battery life!', 5, 1);
+INSERT INTO Review(reviewContent, rating, productId) VALUES('Lags sometimes.', 3, 1);
+INSERT INTO Review(reviewContent, rating, productId) VALUES('Perfect for my daily tasks!', 4, 2);
+INSERT INTO Review(reviewContent, rating, productId) VALUES('Bit pricey, but worth it.', 4, 2);
+INSERT INTO Review(reviewContent, rating, productId) VALUES('Awesome gaming experience!', 5, 3);
+INSERT INTO Review(reviewContent, rating, productId) VALUES('Needs more exclusive games.', 4, 3);
+
+
